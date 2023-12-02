4223e9bdcf24689bdd9d48b91fd11b72774ee57c
Sassi <sassi.bentiba@vaxa.fr>
2023-12-02 20:58:15 +0100

    Creation of ExempleOfInputDay.java

---
+++ src/main/java/com/enjoy/ExempleOfInputDay.java
@@ -0,0 +1,12 @@
+package com.enjoy;
+
+public class ExempleOfInputDay {
+
+    public String getInput() {
+        return this.input;
+    }
+
+    private String input = """
+            
+            """;
+}
